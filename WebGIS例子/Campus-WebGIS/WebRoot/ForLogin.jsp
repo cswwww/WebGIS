5081982f2f228ee308e40cc1f93b85581fa70339
ReBeX <420659880@qq.com>
2021-09-07 22:01:58 +0800

    删除无关项目

--- WebGIS例子/Campus-WebGIS/WebRoot/ForLogin.jsp
+++
@@ -1,6 +0,0 @@
-<%@ page language="java" import="java.util.*" pageEncoding="UTF-8"%>
-
-<% 
-			session.setAttribute("flag", true);
-        	pageContext.forward("map.jsp");
-%>
--- WebGIS例子/Campus-WebGIS/WebRoot/assets/js/helpers.js
+++
@@ -1,9 +0,0 @@
-var geojsonToFeatures = function geojsonToFeatures(fc, options) {
-  var options = options || {};
-  // Declare a formatter to read GeoJSON
-  var format = new ol.format.GeoJSON();
-
-  // Read GeoJSON features
-  var features = format.readFeatures(fc, options);
-  return features;
-};
